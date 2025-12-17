29b2ca4eda64dc3b828d06b398ddb842ef1ffe02
Ivan Dubrovin <ivandub2307@gmail.com>
2025-12-18 02:43:48 +0700

    refactor: сделать items - константой, объявить явно функции обработчики, выровнять отступы

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let items = [
-	"Сделать проектную работу",
-	"Полить цветы",
-	"Пройти туториал по Реакту",
-	"Сделать фронт для своего проекта",
-	"Прогуляться по улице в солнечный день",
-	"Помыть посуду",
+const items = [
+  "Сделать проектную работу",
+  "Полить цветы",
+  "Пройти туториал по Реакту",
+  "Сделать фронт для своего проекта",
+  "Прогуляться по улице в солнечный день",
+  "Помыть посуду",
 ];
 
 // Необходимые элементы разметки
@@ -14,91 +14,96 @@ const inputElement = document.querySelector(".to-do__input");
 
 // Загружает задачи
 function loadTasks() {
-	const tasks = JSON.parse(localStorage.getItem("tasks"));
-	if (tasks && tasks.length > 0) {
-		return tasks;
-	}
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  if (tasks && tasks.length > 0) {
+    return tasks;
+  }
 
-	return items;
+  return items;
 }
 
 // Создает задачу и настраивает обработчики событий для задачи
 function createItem(item) {
-	const template = document.getElementById("to-do__item-template");
-	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
+  const template = document.getElementById("to-do__item-template");
+  const clone = template.content.querySelector(".to-do__item").cloneNode(true);
   const textElement = clone.querySelector(".to-do__item-text");
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+  const duplicateButton = clone.querySelector(
+    ".to-do__item-button_type_duplicate"
+  );
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
-	textElement.textContent = item;
+  textElement.textContent = item;
 
-	// Удаление задачи
-	deleteButton.addEventListener("click", () => {
-		clone.remove();
+  function handleDelete() {
+    clone.remove();
+    const items = getTasksFromDOM();
+    saveTasks(items);
+  }
 
-		const items = getTasksFromDOM();
-		saveTasks(items);
-	});
+  function handleDuplicate() {
+    const itemName = textElement.textContent;
+    const newItem = createItem(itemName);
+    listElement.prepend(newItem);
+    const items = getTasksFromDOM();
+    saveTasks(items);
+  }
 
-	// Копирование задачи
-	duplicateButton.addEventListener("click", () => {
-		const itemName = textElement.textContent;
-		const newItem = createItem(itemName);
+  function handleEdit() {
+    textElement.setAttribute("contenteditable", "true");
+    textElement.focus();
+  }
 
-		listElement.prepend(newItem);
+  function handleTextBlur() {
+    textElement.setAttribute("contenteditable", "false");
+    const items = getTasksFromDOM();
+    saveTasks(items);
+  }
 
-		const items = getTasksFromDOM();
-		saveTasks(items);
-	});
+  // Удаление задачи
+  deleteButton.addEventListener("click", handleDelete);
 
-	// Редактирование задачи
-	editButton.addEventListener("click", () => {
-		textElement.setAttribute("contenteditable", "true");
-		textElement.focus();
-	});
+  // Копирование задачи
+  duplicateButton.addEventListener("click", handleDuplicate);
 
-	// Текст задачи изменен
-	textElement.addEventListener("blur", () => {
-		textElement.setAttribute("contenteditable", "false");
+  // Редактирование задачи
+  editButton.addEventListener("click", handleEdit);
 
-		const items = getTasksFromDOM();
-		saveTasks(items)
-	});
+  // Текст задачи изменен
+  textElement.addEventListener("blur", handleTextBlur);
 
-	return clone;
+  return clone;
 }
 
 // Получение массива задач из DOM
 function getTasksFromDOM() {
-	const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
-	const tasks = [];
+  const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+  const tasks = [];
 
-	itemsNamesElements.forEach(item => tasks.push(item.textContent));
+  itemsNamesElements.forEach((item) => tasks.push(item.textContent));
 
-	return tasks;
+  return tasks;
 }
 
 // Сохранение задач в localStorage
 function saveTasks(tasks) {
-	localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-items = loadTasks();
+const tasks = loadTasks();
 
-items.forEach(item => listElement.append(createItem(item)));
+tasks.forEach((task) => listElement.append(createItem(task)));
 
 // Обработка отправки формы (добавления задачи)
-formElement.addEventListener("submit", evt => {
-	evt.preventDefault();
+formElement.addEventListener("submit", (evt) => {
+  evt.preventDefault();
 
-	const task = inputElement.value;
+  const task = inputElement.value;
 
-	listElement.prepend(createItem(task));
+  listElement.prepend(createItem(task));
 
-	items = getTasksFromDOM();
-	saveTasks(items);
+  items = getTasksFromDOM();
+  saveTasks(items);
 
-	formElement.reset();
+  formElement.reset();
 });
-
